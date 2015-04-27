f4670dbdc84bfa92e8b1618bcac2f1f44431b077
Mangatome <Mangatome@users.noreply.github.com>
2015-04-27 21:33:42 +0200

    Updated assembly version: 1.0.20.0

--- Geowigo/Properties/AssemblyInfo.cs
+++ Geowigo/Properties/AssemblyInfo.cs
@@ -32,6 +32,6 @@ using System.Resources;
 //
 // Vous pouvez spécifier toutes les valeurs ou indiquer les numéros de révision et de build par défaut 
 // en utilisant '*', comme indiqué ci-dessous :
-[assembly: AssemblyVersion("1.0.19.1")]
-[assembly: AssemblyFileVersion("1.0.19.1")]
+[assembly: AssemblyVersion("1.0.20")]
+[assembly: AssemblyFileVersion("1.0.20")]
 [assembly: NeutralResourcesLanguageAttribute("en")]
